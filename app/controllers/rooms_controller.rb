730cea4ac625d1a9211e0584f5a4f1d5accaad9c
Miriam Tocino <miriam.tocino@gmail.com>
2016-09-28 13:40:12 +0200

    One-to-Many: Controllers

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,45 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def show
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+
+    if @room.save
+      redirect_to @room, notice: "Room successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: "Updated..."
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
+    def room_params
+      params.require(:room).permit(:home_type, :room_type, :accommodate, :bedroom_count, :bathroom_count, :listing_name, :description, :address, :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet, :price, :active)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+  resources :rooms
 end
