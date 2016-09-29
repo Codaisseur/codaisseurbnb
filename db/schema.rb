c134a3d645b23c27b583aee6d7e6fb7289246685
Miriam Tocino <miriam.tocino@gmail.com>
2016-09-29 17:29:43 +0200

    One-to-One Association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928150706) do
+ActiveRecord::Schema.define(version: 20160929144219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "bio"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "home_type"
