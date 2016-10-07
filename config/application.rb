0b83197f9e8568086abd6ff494d0353e0b449db2
Miriam Tocino <miriam.tocino@gmail.com>
2016-10-07 16:46:38 +0200

    Add ActiveRecord connection

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module Codaisseurbnb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    console do
+      ActiveRecord::Base.connection
+    end
   end
 end
